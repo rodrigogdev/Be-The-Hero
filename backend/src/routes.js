d0180fb337065c746f70284dbdad502dc7bd935f
Rodrigo <rodrigogoncalves121@gmail.com>
2021-12-06 12:56:22 -0300

    React

---
+++ backend/src/routes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const IncidentController = require("./controllers/IncidentController");
+const OngController = require('./controllers/OngController');
+const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
+
+const routes = express.Router();
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+routes.post('/profile', SessionController.create);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = routes;
